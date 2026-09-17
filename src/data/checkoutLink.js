c1909f136bf890912243b97f0d3a59fcc815869d
ThissMadd <mehdimarih10@gmail.com>
2026-09-17 21:27:26 +0100

    Update website

--- src/data/checkoutLink.js
+++ src/data/checkoutLink.js
@@ -1,5 +1,5 @@
 const SHOPIFY_VARIANT_ID = '58031314698622';
 
 export function checkoutLinkFor(qty){
-  return `https://pgnyah-we.myshopify.com/cart/${SHOPIFY_VARIANT_ID}:${qty}`;
+  return `https://shoopron.myshopify.com/cart/${SHOPIFY_VARIANT_ID}:${qty}`;
 }
